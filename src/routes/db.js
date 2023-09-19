5ddc8df397050b029033b2416506e978d2de0ea7
oscar emilio <oxcaremlio123@gmail.com>
2023-09-18 18:14:52 -0600

    cambios en el archivo db para las llamadas de los datos almacenados

--- src/routes/db.js
+++ src/routes/db.js
@@ -2,12 +2,16 @@ import cliente from 'pg';
 
 const { Pool } = cliente;
 
-const pool = new Pool ({
+const pool = new Pool({
+    /*
     user: 'postgres',
     host: 'localhost',
     database: 'back',
     password: '123456',
     port:5432
+    */
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
 
 export {
